87859e859ac509e92e1bd618ef7c77e9dd540453
roxerg <roxguda@gmail.com>
2021-04-17 18:51:12 +0100

    timeout fixed lol

--- bot.py
+++ bot.py
@@ -346,7 +346,7 @@ async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
                )
              ])
 async def _collage(ctx, username="", dimensions="3x3", period="7day"):
-    await ctx.respond()
+    await ctx.defer()
     username=str(username)
     
     if username == "":
@@ -361,4 +361,4 @@ async def _collage(ctx, username="", dimensions="3x3", period="7day"):
 
     pass
 
-#client.run(TOKEN)
+client.run(TOKEN)

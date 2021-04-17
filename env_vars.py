84244ade6d0699401c642f72d4f0a4995cc00c25
roxerg <roxguda@gmail.com>
2021-04-17 22:28:33 +0100

    aaa

--- env_vars.py
+++ env_vars.py
@@ -6,5 +6,4 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 FM_API_KEY = os.getenv('LASTFM_API_KEY')
 APP_ID = os.getenv('APP_ID')
-GUILDS = os.getenv("GUILDS")
-GUILD_IDS = [int(guild) for guild in GUILDS.split(",")]
+GUILD_IDS = [int(guild) for guild in os.getenv("GUILDS").split(",")]

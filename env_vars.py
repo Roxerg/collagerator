9f7e871f98700dc424fadb1b3b2468b92bf0d911
roxerg <roxguda@gmail.com>
2021-10-06 15:35:27 +0100

    linting for a change

--- env_vars.py
+++ env_vars.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-FM_API_KEY = os.getenv('LASTFM_API_KEY')
-APP_ID = os.getenv('APP_ID')
+TOKEN = os.getenv("DISCORD_TOKEN")
+FM_API_KEY = os.getenv("LASTFM_API_KEY")
+APP_ID = os.getenv("APP_ID")
 GUILD_IDS = [int(guild) for guild in os.getenv("GUILDS").split(",")]
